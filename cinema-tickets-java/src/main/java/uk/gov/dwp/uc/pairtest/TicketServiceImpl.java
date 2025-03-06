87097e74894256aec37b79c3aa0dd750b2937938
alex-sumy-ua <plachkovskyy@gmail.com>
2025-03-06 18:25:17 +0000

    implementation: accountIsValid, ticketRequestIsValid, purchaseTickets

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -10,7 +10,30 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        if (!accountIsValid(accountId) ||
+            !ticketRequestIsValid(ticketTypeRequests)) {
+            throw new InvalidPurchaseException();
+        }
 
+        // Continue with payment and seat reservation logic...
+
+    }
+
+    private boolean accountIsValid(Long accountId) {
+        return accountId > 0;
+    }
+
+    private boolean ticketRequestIsValid(TicketTypeRequest... ticketTypeRequests) {
+        int totalTickets = 0;
+        int adultTickets = 0;
+
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            totalTickets += request.getNoOfTickets();
+            if (request.getTicketType() == TicketTypeRequest.Type.ADULT) {
+                adultTickets += request.getNoOfTickets();
+            }
+        }
+            return adultTickets > 0 && totalTickets <= 25;
     }
 
 }
