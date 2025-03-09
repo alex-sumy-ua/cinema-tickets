5a73821c81d8d94e91c0e3abf82ab66649402326
alex-sumy-ua <plachkovskyy@gmail.com>
2025-03-09 00:22:32 +0000

    improvement: Exceptions handling

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -2,4 +2,7 @@ package uk.gov.dwp.uc.pairtest.exception;
 
 public class InvalidPurchaseException extends RuntimeException {
 
+    public InvalidPurchaseException(String s) {
+        super(s); // Passes the message to the RuntimeException class
+    }
 }
